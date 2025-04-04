78607dbb8be433c56a3913a415c92aee92c4e2d7
Jeff Ripke <jripke74@gmail.com>
2025-04-04 07:00:20 -0700

    add all players cards

--- script.js
+++ script.js
@@ -26,6 +26,25 @@ const footballTeam = {
   ],
 };
 
-const team = document.querySelector("#team");
-const year = document.querySelector("#year");
-const headCoach = document.querySelector("#head-coach");
+document.getElementById("team").textContent = footballTeam.team;
+document.getElementById("year").textContent = footballTeam.year;
+document.getElementById("head-coach").textContent = footballTeam.headCoach;
+
+const playerCards = document.getElementById("player-cards");
+
+const playerCardsContainer = document.getElementById("player-cards");
+
+footballTeam.players.forEach((player) => {
+  const cardDiv = document.createElement("div");
+  const nameHeading = document.createElement("h2");
+  const positionPara = document.createElement("p");
+
+  nameHeading.textContent = player.isCaptain ? `${player.name}` : player.name;
+  positionPara.textContent = `${player.name}`;
+
+  cardDiv.classList.add("player-card");
+  cardDiv.appendChild(nameHeading);
+  cardDiv.appendChild(positionPara);
+
+  playerCardsContainer.appendChild(cardDiv);
+});
